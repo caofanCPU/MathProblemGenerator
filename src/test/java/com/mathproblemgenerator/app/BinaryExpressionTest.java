fa0b8cecf38852d0cd6301f66e2041702188882a
Tobi Lehman <tobi.lehman@gmail.com>
2014-02-16 20:20:24 -0800

    add more BinaryExpression tests

--- src/test/java/com/mathproblemgenerator/app/BinaryExpressionTest.java
+++ src/test/java/com/mathproblemgenerator/app/BinaryExpressionTest.java
@@ -14,6 +14,9 @@ public class BinaryExpressionTest extends TestCase
         BinaryExpression binexp = new BinaryExpression("-", 1.5, 1.0);
         assertEquals("1.5 - 1", binexp.toString());
         assertEquals(0.5, binexp.eval());
+
+        assertEquals(0.0, (new BinaryExpression("*", 0.0, 23.123)).eval() );
+        assertEquals(Double.POSITIVE_INFINITY, (new BinaryExpression("/", 1.0, 0.0)).eval() );
     }
 }
 
